4cadcd19423cbe8e6014efcb6e8532389464f792
Stjoo0925 <stjoo0925@gmail.com>
2024-06-17 12:52:17 +0900

    docs: redirect & MnV redirect

--- chap03-view-resolver/src/main/java/com/ohgiraffers/chap03viewresolver/MainController.java
+++ chap03-view-resolver/src/main/java/com/ohgiraffers/chap03viewresolver/MainController.java
@@ -2,13 +2,24 @@ package com.ohgiraffers.chap03viewresolver;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-    @RequestMapping(value = { "/main"})
+    @RequestMapping(value = { "/"})
     public String main() {
         return "main";
+    }
+
+    /*
+    *   RequestToViewNameTranslator(interface)
+    *   spring 에서 반환 타입이 void 인 경우 요청 url 을 기반으로 뷰를 해석하려고하는데
+    *   이때 RequestToViewNameTranslator 를 사용하여 url 을 뷰의 이름으로 변환한다.
+    * */
+
+
+    @RequestMapping(value= "/main")
+    public void mainPage() {
+
     }
 }
